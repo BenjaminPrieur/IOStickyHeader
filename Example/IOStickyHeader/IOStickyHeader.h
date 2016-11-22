c1e46abaa54cc179375246071e10dab51579c70e
Benjamin Prieur <ben@MacBook-Pro-de-Benjamin.local>
2016-11-22 21:19:08 +0100

    Add carthage support

---
+++ Example/IOStickyHeader/IOStickyHeader.h
@@ -0,0 +1,19 @@
+//
+//  IOStickyHeader.h
+//  IOStickyHeader
+//
+//  Created by Benjamin Prieur on 22/11/2016.
+//  Copyright © 2016 Benjamin Prieur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IOStickyHeader.
+FOUNDATION_EXPORT double IOStickyHeaderVersionNumber;
+
+//! Project version string for IOStickyHeader.
+FOUNDATION_EXPORT const unsigned char IOStickyHeaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOStickyHeader/PublicHeader.h>
+
+
